4fa0c924f4d0e612d52be44d42ee51b4d1f2b817
ginaa07 <1123150124@global.ac.id>
2025-12-11 19:12:06 +0700

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+const supabaseUrl = 'https://abrcktepwhjxvljdplay.supabase.co';
+const supabasekey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFicmNrdGVwd2hqeHZsamRwbGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MjMyOTksImV4cCI6MjA4MDk5OTI5OX0.KM2Nz7zEfnILO9sSV8vSngmj3XNBDPhGOI8UkmD6pHU';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabasekey);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "project supabase",
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
-      ),
+    title: 'Supabase Foto', 
+    home: MyHomePage(),
     );
   }
 }
